2b2cd7dec9bab3d105dc87176ef9ba86e7c2d63c
Michaun Pierre <michaun.pierre@bison.howard.edu>
2019-08-28 19:22:51 +0000

    Started argument parser

---
+++ margin.py
@@ -0,0 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="Process text file with given margins.")
+parser.addargument('-i', dest='input_file', type=string, nargs=1, help='Text input file.')
+parser.addargument()
+def main()
+
+
+
+if __name__== "__main__":
+  main()
