2cb52b7ce45cab7bc1aef7a58a896e1cfd019063
Michaun Pierre <michaun.pierre@bison.howard.edu>
2019-09-06 06:42:09 +0000

    Finished V1 of the margin script

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # SWE-Proj1
+
+## Requirements/Assumptions
+
+* We are assuming English letters only, and a default 12 pt font size for the text input.
+* We are assuming that a sentence is at least two characters with one of them being a question mark, exclamation point, or period.
+* We are assuming that no negative integers are given as input.
+* We are assuming that we can just output the adjusted text to the terminal, and not a file
+
+Must specify input text file with -i flag
+Must specify width of left margin with -l flag
+Must specify width of right margin with the -r flag
+
+
+## Algorithm
+1.) Open the input file and tokenize all of the words in the file
+2.) Iterate through each word and do the following
+3.) Check the length of the word
+4.) Check the last character of the word
+5.) Subtract the length of the word from the available space on the line
+6.) If the difference is greater than or equal to zero print the word on the line, otherwise move to the next line and print the word
+7.) If the length of the word was greater than or equal to two and last character of the word was a period, exclamation point or question mark print two spaces while respecting margins. Otherwise, print one space while respecting margins.
+
+## Test Cases
+My test cases were different types of sentences that I wrote in my test file. Some sentences included multiple symbols, capitalized words, and long words.
--- margin.py
+++ margin.py
@@ -1,11 +1,72 @@
+#!/usr/bin/env python3
 import argparse
+import math
+import tokenize
 
 parser = argparse.ArgumentParser(description="Process text file with given margins.")
-parser.addargument('-i', dest='input_file', type=string, nargs=1, help='Text input file.')
-parser.addargument()
-def main()
+parser.add_argument('-i', dest='input_file', type=str, nargs=1, help='Text input file.')
+parser.add_argument('-o', dest='output_file', type=str, nargs=1, help='Text output file.')
+parser.add_argument('-l', dest='l_margin', type=int, nargs=1, help='Width of left margin in inches.')
+parser.add_argument('-r', dest='r_margin', type=int, nargs=1, help='Width of left margin in inches.')
 
+IN_TO_PT = 64/11
+punctuation = ['?', '.', '!']
 
+def main():
+  args = parser.parse_args()
+  for i in range(1, 81):
+    if i == 80:
+      print('c')
+    else: 
+      print('c', end= "")
+      
+  file_content = open(args.input_file[0], 'r')
+  lines = file_content.readlines()
+
+  tokens = []
+  for line in lines:
+    line_tokens = line.split()
+    for token in line_tokens:
+      tokens.append(token)
+      
+  l_characters = math.floor(args.l_margin[0] * IN_TO_PT)
+  r_characters = math.floor(args.r_margin[0] * IN_TO_PT)
+  margin_space = l_characters + r_characters
+  text_space = 80 - margin_space
+  curr_text_space = text_space
+  
+  for i in range(l_characters):
+      print(' ', end='')
+  for token in tokens:
+    sentence_end = token[-1] in punctuation and len(token) > 1
+    word_len = len(token)
+    rem_space = curr_text_space - word_len
+      
+    if sentence_end:
+      if rem_space >= 2:
+        print(token, end='  ')
+        curr_text_space -= (word_len + 2)
+      else:
+        for i in range(r_characters):
+          print(' ', end='')
+        print('\n')
+        for i in range(l_characters):
+          print(' ', end='')
+        print(token, end='  ')
+        curr_text_space = text_space - (word_len + 2)
+        
+    else:
+      if rem_space >= 1:
+        print(token, end=' ')
+        curr_text_space -= (word_len + 1)
+      else:
+        for i in range(r_characters):
+          print(' ', end='')
+        print('\n')
+        for i in range(l_characters):
+          print(' ', end='')
+        print(token, end=' ')
+        curr_text_space = text_space - (word_len + 1)
 
 if __name__== "__main__":
   main()
